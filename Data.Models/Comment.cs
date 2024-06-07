56956d4d298cfd990a39a5f219adb20a6491e565
Zane Neurothrone <54468049+neurothrone@users.noreply.github.com>
2024-06-07 16:52:55 +0200

    initial commit

---
+++ Data.Models/Comment.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Data.Models;
+
+public class Comment
+{
+    public string? Id { get; set; }
+    public required string BlogPostId { get; set; }
+    public DateTime Date { get; set; }
+
+    [Required]
+    public string Text { get; set; } = string.Empty;
+
+    [Required]
+    public string Name { get; set; } = string.Empty;
+}
---
+++ SharedComponents/CustomCssClassProvider.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+
+namespace SharedComponents;
+
+// We specified that type must inherit from FieldCssClassProvider and must have a constructor without parameters.
+// The component inherits from ComponentBase, which makes it possible to place the component inside a Blazor component.
+public class CustomCssClassProvider<ProviderType> : ComponentBase where ProviderType : FieldCssClassProvider, new()
+{
+    [CascadingParameter]
+    EditContext? CurrentEditContext { get; set; }
+
+    public ProviderType Provider { get; set; } = new();
+
+    protected override void OnInitialized()
+    {
+        if (CurrentEditContext == null)
+        {
+            throw new InvalidOperationException(
+                $"{nameof(CustomCssClassProvider<ProviderType>)} requires a cascading parameter of type {nameof(EditContext)}. For example, you can use {nameof(CustomCssClassProvider<ProviderType>)} inside an EditForm.");
+        }
+
+        CurrentEditContext.SetFieldCssClassProvider(Provider);
+    }
+}
